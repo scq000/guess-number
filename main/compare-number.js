86007dfc44561cc3b91eb187ae68c24798437e55
scq000 <scq000@hotmail.com>
2015-07-30 16:13:11 +0800

    format code

--- main/compare-number.js
+++ main/compare-number.js
@@ -1,19 +1,18 @@
 'use strict';
 
-function CompareNumber (){
-}
+function CompareNumber() {}
 
-CompareNumber.prototype.compare =function (answer, input){
+CompareNumber.prototype.compare = function(answer, input) {
   var countA = 0;
   var countB = 0;
 
-  answer.split('').forEach(function(num,index) {
+  answer.split('').forEach(function(num, index) {
     var findIndex = input.indexOf(num);
-    countA += (findIndex === index)? 1: 0;
-    countB += (findIndex !== -1 && findIndex !== index)? 1:0;
+    countA += (findIndex === index) ? 1 : 0;
+    countB += (findIndex !== -1 && findIndex !== index) ? 1 : 0;
   });
 
-  return  countA +'A' + countB + 'B';
+  return countA + 'A' + countB + 'B';
 };
 
 module.exports = CompareNumber;
